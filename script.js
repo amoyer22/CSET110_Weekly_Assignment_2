c75f2214cefa23ad27ca59037b6d05ea1a659e04
Ailish <snickrrdoodles@gmail.com>
2024-10-16 17:12:57 -0400

    Add to cart function works properly

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function album1Add(){
     console.log("album 1")
-    let cartView= `<div class="cart-row">
-                        <div class="cart-item cart-column">
-                            <img class="cart-item-image" src="Images/Album 1.png" width="100" height="100">
-                            <span class="cart-item-title">Album 1</span>
-                        </div>
-                        <span class="cart-price cart-column">$12.99</span>
-                        <div class="cart-quantity cart-column">
-                            <input class="cart-quantity-input" type="number" value="1">
-                            <button class="btn btn-danger" type="button" onclick="remove()">REMOVE</button>
-                        </div>
-                    </div>`
-    document.createElement("div")
+    let cartView = `
+    <div class="cart-row">
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="Images/Album 1.png" width="100" height="100">
+            <span class="cart-item-title">Album 1</span>
+        </div>
+        <span class="cart-price cart-column">$12.99</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button" onclick="remove()">REMOVE</button>
+        </div>
+    </div>`
+    let div = document.createElement("div")
     div.innerHTML = cartView
-    document.getElementsByClassName("cart-items").appendChild(div)
+    document.getElementsByClassName("cart-items")[0].appendChild(div)
 }
 
 
