cdf958cf97982fd53784d47dfb4a294fe0176fef
Ailish <snickrrdoodles@gmail.com>
2024-10-16 09:14:53 -0400

    Button function tests

---
+++ script.js
@@ -0,0 +1,31 @@
+function album1Add(){
+    console.log("album 1")
+}
+
+function album2Add(){
+    console.log("album 2")
+}
+
+function album3Add(){
+    console.log("album 3")
+}
+
+function album4Add(){
+    console.log("album 4")
+}
+
+function tShirtAdd(){
+    console.log("t-shirt")
+}
+
+function coffeeCupAdd(){
+    console.log("coffee cup")
+}
+
+function remove(){
+    console.log("remove")
+}
+
+function purchase(){
+    console.log("purchase")
+}
