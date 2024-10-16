5bc3336670833bdc221e1f9d16db3c3679d270d3
Ailish <snickrrdoodles@gmail.com>
2024-10-16 10:58:39 -0400

    Appending div to cart setup

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 function album1Add(){
     console.log("album 1")
+    let cartView= `<div class="cart-row">
+                        <div class="cart-item cart-column">
+                            <img class="cart-item-image" src="Images/Album 1.png" width="100" height="100">
+                            <span class="cart-item-title">Album 1</span>
+                        </div>
+                        <span class="cart-price cart-column">$12.99</span>
+                        <div class="cart-quantity cart-column">
+                            <input class="cart-quantity-input" type="number" value="1">
+                            <button class="btn btn-danger" type="button" onclick="remove()">REMOVE</button>
+                        </div>
+                    </div>`
+    document.createElement("div")
+    div.innerHTML = cartView
+    document.getElementsByClassName("cart-items").appendChild(div)
 }
+
 
 function album2Add(){
     console.log("album 2")
 }
 
+
 function album3Add(){
     console.log("album 3")
 }
 
+
 function album4Add(){
     console.log("album 4")
 }
+
 
 function tShirtAdd(){
     console.log("t-shirt")
@@ -22,9 +40,11 @@ function coffeeCupAdd(){
     console.log("coffee cup")
 }
 
+
 function remove(){
     console.log("remove")
 }
+
 
 function purchase(){
     console.log("purchase")
